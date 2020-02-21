165891c9bbb2311445a7b5503255e8dcafdb73c6
Kirsanov Viacheslav <kirsanovs@yandex.ru>
2020-02-22 00:12:43 +0300

    Шаг 3. Приветствие.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+
+import { greet } from '../src/index.js';
+
 console.log('Welcome to the Brain Games!');
+greet();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+export const greet = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + userName + '!');
+
+    return;
+};
