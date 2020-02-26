33e5cc739fe4c6e1a4347b8e7ec2ea12d8c5ad96
Kirsanov Viacheslav <kirsanovs@yandex.ru>
2020-02-26 22:22:21 +0300

    Шаг 4. Подключение CodeClimate, Eslint, Github Actions.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { greet } from '../src/index.js';
+import greet from '../src/index.js';
 
 console.log('Welcome to the Brain Games!');
 greet();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import readlineSync from 'readline-sync';
+import { readlineSync } from 'readline-sync';
 
-export const greet = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
-
-    return;
+export default () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
